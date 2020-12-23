e258eaf70892c030139187ec343e1caadc1b0096
zeeshahali <z3eshahali@gmail.com>
2020-12-23 22:36:39 +0500

    added users, teacher and student

--- lms_system/src/com/lms/Student.java
+++ lms_system/src/com/lms/Student.java
@@ -1,4 +1,15 @@
 package com.lms;
 
-public class Student {
+import java.util.ArrayList;
+
+public class Student extends User{
+    String rollNumber;
+    int batch;
+    ArrayList<Registered> courses = new ArrayList<>();
+
+    public Student(String rollNumber, int batch, String name){
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.batch = batch;
+    }
 }
