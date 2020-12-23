1d825b55f4e089d94d7d12802b3783188092cf74
zeeshahali <z3eshahali@gmail.com>
2020-12-23 22:36:39 +0500

    added users, teacher and student

--- lms_system/src/com/lms/Teacher.java
+++ lms_system/src/com/lms/Teacher.java
@@ -1,4 +1,14 @@
 package com.lms;
 
-public class Teacher {
+import java.util.ArrayList;
+
+public class Teacher extends User{
+    String username;
+    ArrayList<Registered> assignedCourses = new ArrayList<Registered>();
+
+    public Teacher(String username, String name, ArrayList<Registered> assignedCourses){
+        this.username = username;
+        this.name = name;
+        this.assignedCourses.addAll(assignedCourses);
+    }
 }
--- lms_system/src/com/lms/User.java
+++ lms_system/src/com/lms/User.java
@@ -1,4 +1,7 @@
 package com.lms;
 
 public class User {
+    String name;
+
+    public String getName() {return this.name; }
 }
